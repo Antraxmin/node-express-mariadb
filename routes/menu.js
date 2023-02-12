00d4172087840c510f0043bdf894d639160b11e7
Antraxmin <antraxmin@naver.com>
2023-02-12 12:56:40 +0900

    add menu route path

---
+++ routes/menu.js
@@ -0,0 +1,13 @@
+const router = express.Router();
+
+// GET /menu
+router.get("/", (req, res) => {
+
+});
+
+// GET /menu/{menuId}
+router.get("/:menuId", (req, res) => {
+
+});
+
+module.exports = router;
