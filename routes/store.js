662dfee58e636485986bd7447dc1ac2083228e62
Antraxmin <antraxmin@naver.com>
2023-02-12 12:54:07 +0900

    add store route path

---
+++ routes/store.js
@@ -0,0 +1,15 @@
+const router = express.Router();
+
+// GET /store
+router.get("/", (req, res) => {
+
+});
+
+// GET /store/{storeId}
+router.get("/:storeId", (req, res) => {
+
+});
+
+
+
+module.exports = router;
